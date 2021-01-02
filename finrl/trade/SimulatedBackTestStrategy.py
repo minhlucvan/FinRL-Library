f6a4f8230b1a17e90c98a8ecdd1074595a097d20
Minh Luc Van <luk.mink@gmail.com>
2021-01-02 23:43:00 +0700

    update

--- finrl/trade/SimulatedBackTestStrategy.py
+++ finrl/trade/SimulatedBackTestStrategy.py
@@ -1,34 +1,22 @@
-import pandas as pd
-from backtesting.test import SMA
-from backtesting.lib import SignalStrategy, TrailingStrategy
+import numpy as np
+from backtesting import Backtest, Strategy
 
+class SimulatedBackTestStrategy(Strategy):
 
-class SimulatedBackTestStrategy(SignalStrategy,
-               TrailingStrategy):
-    n1 = 10
-    n2 = 25
-    
-    def init(self):
-        # In init() and in next() it is important to call the
-        # super method to properly initialize the parent classes
-        super().init()
-        
-        # Precompute the two moving averages
-        sma1 = self.I(SMA, self.data.Close, self.n1)
-        sma2 = self.I(SMA, self.data.Close, self.n2)
-        
-        # Where sma1 crosses sma2 upwards. Diff gives us [-1,0, *1*]
-        signal = (pd.Series(sma1) > sma2).astype(int).diff().fillna(0)
-        signal = signal.replace(-1, 0)  # Upwards/long only
-        
-        # Use 95% of available liquidity (at the time) on each order.
-        # (Leaving a value of 1. would instead buy a single share.)
-        entry_size = signal * .95
-                
-        # Set order entry sizes using the method provided by 
-        # `SignalStrategy`. See the docs.
-        self.set_signal(entry_size=entry_size)
-        
-        # Set trailing stop-loss to 2x ATR using
-        # the method provided by `TrailingStrategy`
-        self.set_trailing_sl(2)
+    def init(self):        
+        # Prepare empty, all-NaN forecast indicator
+        self.forecasts = self.I(lambda: np.repeat(np.nan, len(self.data)), name='forecast')
+
+    def next(self):
+        # Proceed only with out-of-sample data. Prepare some variables
+        action = self.data.actions[0][0]
+
+        forecast = action
+
+        # Update the plotted "forecast" indicator
+        self.forecasts[-1] = forecast
+
+        if forecast > 0:
+            self.buy(size=forecast)
+        elif forecast < 0:
+            self.sell(size=forecast)
